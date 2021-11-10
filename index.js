50a4e7cf7ae1e4ad39d26d4af47efad9ac05edc1
Ibrahim193084 <ibrahimibr193084@gmail.com>
2021-11-10 07:09:34 +0600

    second-commit

--- index.js
+++ index.js
@@ -11,10 +11,34 @@ app.use(cors())
 app.use(express.json())
 
 //database user information
-
-// console.log(uri)
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dy9rj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
 
 // connecting with mongodb
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("superbike");
+      const cycleCollection = database.collection("cycle");
+       //Get api for cycle collection
+       app.get('/cycles', async(req, res)=>{
+        const cursor = cycleCollection.find({});
+        const cycles = await cursor.toArray();
+        res.send(cycles)
+    })
+
+
+     
+
+
+  
+    } finally {
+    //   await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 
 app.get('/',(req, res)=>{
     res.send('Assingnment 12 server')
